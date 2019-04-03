3b23b3f20f05d8ed62e4aa51106244e60282eed0
Nicolas Wyler <nwyler@modusagency.com>
2019-04-03 17:38:45 -0300

    adding new component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 import Fragments from './private_components/fragments';
 
 // import components 
+import Test  from './components/Test/Test;'
+     
 import Test  from './components/Test/Test
     
 import Accordion from './components/Accordion/Accordion';
@@ -14,6 +16,9 @@ class App extends Component {
     return (
       <div className="App">
         <Fragments>
+
+        <Test name="Nicolas" bgColor="#fe3e4e" />
+    
 
         <Test name="Nicolas" bgColor="#fe3e4e" />
     
