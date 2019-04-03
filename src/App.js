86ba2b99d07806d8175d0dc7bcc61337ed41a231
Nicolas Wyler <nwyler@modusagency.com>
2019-04-03 18:59:07 -0300

    adding new component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 // import private components
 import Fragments from './private_components/fragments';
 
-// import components      
+// import components 
+import Test  from './components/Test/Test';
+          
 import Accordion from './components/Accordion/Accordion';
 
 
@@ -11,7 +13,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Fragments>   
+        <Fragments>
+
+        <Test name="Nicolas" bgColor="#fe3e4e" />
+       
           <Accordion question="What is that?" answer="That's an accordion" />
         </Fragments>
       </div>
